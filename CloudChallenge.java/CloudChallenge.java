9381bb93ed8ed667c6eea8a750a5da06bd6a429a
Paul <51698370+KingRocker@users.noreply.github.com>
2019-10-18 10:07:42 +0200

    Add files via upload

---
+++ CloudChallenge.java/CloudChallenge.java
@@ -0,0 +1,12 @@
+public class CloudChallenge
+{
+
+     public static void main(String []args)
+     {
+         float cloud = 1.276459000f;
+         
+         
+         
+        System.out.println(cloud);
+     }
+}
