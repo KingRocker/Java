e71d6e9cc7d1e0a9ceafdaaecf1ca88c814d2b18
Paul <51698370+KingRocker@users.noreply.github.com>
2019-10-18 10:02:53 +0200

    Add files via upload

---
+++ Apple.java/Apple.java
@@ -0,0 +1,12 @@
+public class Apple
+{
+
+     public static void main(String []args)
+     {
+         String apple = "apple";
+         
+         
+         
+        System.out.println(apple);
+     }
+}
